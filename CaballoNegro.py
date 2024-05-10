4dd5065bec5a414db8e3e69fec8b3bd94bb84ad7
S.Sánchez <sscardenas04@gmail.com>
2024-05-10 13:19:08 -0500

    Segundo commit

--- CaballoNegro.py
+++ CaballoNegro.py
@@ -7,31 +7,32 @@ import pygame
 WIDTH, HEIGHT = 800, 800
 SQUARE_SIZE = WIDTH // 8
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+BLACK = (0,0,0)
 GRAY = (211, 211, 211)
-BLUE = (0, 0, 255)
-GREEN = (170, 255, 0)
+BLUE = (0,0,255)
+GREEN = (170,255,0)
 
-# Letters of the columns
+# Letras de las columnas
 COLUMN_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-# Function to draw the chess board
-def drawBoard(screen):
+# Función para dibujar el tablero de ajedrez
+def draw_board(screen):
     for row in range(8):
         for col in range(8):
             color = WHITE if (row + col) % 2 == 0 else GRAY
             pygame.draw.rect(screen, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            
 
-def getColumnLetter(col):
+def get_column_letter(col):
     return COLUMN_LETTERS[col]
 
-def getColumnIndex(letra):
+def get_column_index(letra):
     return COLUMN_LETTERS.index(letra)
 
-def drawPieces(screen, board):
+def draw_pieces(screen, board):
     for row in range(8):
         for col in range(8):
-            square = col, 7 - row
+            square = col, 7-row
             piece = board.piece_at(chess.square(*square))
             if piece is not None:
                 if piece.color:
@@ -43,95 +44,102 @@ def drawPieces(screen, board):
                     img = pygame.transform.scale(img, (SQUARE_SIZE, SQUARE_SIZE))
                     screen.blit(img, (col * SQUARE_SIZE, row * SQUARE_SIZE))
 
-def grafoManager(grafo, move):
-    fromNodo = getNameNodo(move.from_square)
-    toNodo = getNameNodo(move.to_square)
-    if not grafo.has_node(fromNodo):
-        addNodo(grafo, fromNodo)
-    if not grafo.has_node(toNodo):
-        addNodo(grafo, toNodo)
-        addArista(grafo, fromNodo, toNodo)
 
-def getNameNodo(square):
+def GetNameNodo(square):
     to_row = chess.square_rank(square)
     to_col = chess.square_file(square)
-    col = getColumnLetter(to_col)
-    return str(col) + str(to_row)
+    col = get_column_letter(to_col)
+    return str(col)+str(to_row)
 
-def crearGrafo():
+
+def crear_grafo():
     return nx.Graph()
 
-def addNodo(grafo, nodo):
+def agregar_nodo(grafo, nodo):
     grafo.add_node(nodo)
 
-def addArista(grafo, nodo1, nodo2):
+def agregar_arista(grafo, nodo1, nodo2):
     grafo.add_edge(nodo1, nodo2)
 
-def mostrarGrafo(grafo):
+def mostrar_grafo(grafo):
     nx.draw(grafo, with_labels=True, node_color='lightblue', node_size=800, font_size=10)
     plt.show()
 
-def posInicial(board, ficha):
-    casillasConPiezas = board.piece_map()
-    for casilla, pieza in casillasConPiezas.items():
+
+def grafoManager(grafo,move):
+    FromNodo = str(GetNameNodo(move.from_square))
+    ToNodo = str(GetNameNodo(move.to_square))
+    if not grafo.has_node(FromNodo):
+        agregar_nodo(grafo,FromNodo)
+    if not grafo.has_node(ToNodo):
+        agregar_nodo(grafo,ToNodo)
+        agregar_arista(grafo,FromNodo,ToNodo)
+    
+
+def pos_inicial(board, ficha):
+    casillas_con_piezas = board.piece_map()
+    for casilla, pieza in casillas_con_piezas.items():
         if pieza.symbol() == ficha.symbol():
             return chess.square_name(casilla)
 
-def movimientosFicha(board):
-    movPosibles = []
+
+def movimientos_ficha(board):
+    mov_posibles = []
     for move in board.legal_moves:
         to_square = move.to_square
-        # Obtain the row and column index of the destination square
+        # Obtener el índice de fila y columna de la casilla de destino
         to_row = chess.square_rank(to_square)
         to_col = chess.square_file(to_square)
-        movPosibles.append((to_row, to_col))
-    return movPosibles
+        mov_posibles.append((to_row, to_col))
+    return mov_posibles
 
-def moverPieza(grafo, board, row, col):
-    destSquare = chess.square(col, 7 - row)  # Convert the row and column to a square index
-    move = chess.Move.null()  # Initialize a null move by default
-    for legalMove in board.legal_moves:
-        if legalMove.to_square == destSquare:
-            move = legalMove
+def mover_pieza(grafo,board, row, col):
+    dest_square = chess.square(col, 7 - row)  # Convertir las coordenadas de fila y columna a un índice de casilla
+    move = chess.Move.null()  # Inicializar un movimiento nulo por defecto
+    for legal_move in board.legal_moves:
+        if legal_move.to_square == dest_square:
+            move = legal_move
             break
     if move != chess.Move.null():
-        board.push(move)  # Apply the move if it's legal
-        grafoManager(grafo, move)
+        board.push(move)  # Aplicar el movimiento si es legal
+        grafoManager(grafo,move)
         return True
 
 def ganador(board):
     if len(board.piece_map()) == 1:
         return True
 
-def mostrarVictoria(screen, ganador):
-    font = pygame.font.Font(None, 36)  # Define the font and size of the text
-    text = font.render(f"Ganan las {ganador}", True, BLACK)  # Render the text into a surface
-    textRect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-    screen.blit(text, textRect) 
+def mostrar_victoria(screen, ganador):
+    font = pygame.font.Font(None, 36)  # Definir la fuente y el tamaño del texto
+    text = font.render(f"Ganan las {ganador}", True, BLACK)  # Renderizar el texto en una superficie
+    text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2)) 
+    screen.blit(text, text_rect)  
 
-def mostrarStalemate(screen):
-    font = pygame.font.Font(None, 36)  # Define the font and size of the text
-    text = font.render("No hay más movimientos", True, BLACK)  # Render the text into a surface
-    
-    textRect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-    screen.blit(text, textRect) 
+def mostrar_stalemate(screen):
+    font = pygame.font.Font(None, 36)  # Definir la fuente y el tamaño del texto
+    text = font.render("No hay más movimientos", True, BLACK)  # Renderizar el texto en una superficie
+    text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2)) 
+    screen.blit(text, text_rect)  
 
 def stalemate(board):
     return board.is_stalemate()
 
-def getNewPosicion():
-    posicionesCaballo = ["n7", "1n6", "2n5", "3n4", "4n3", "5n2", "6n1", "7n"]
+def GetNewPosicion():
+    posicionesCaballo = ["n7","1n6", "2n5", "3n4", "4n3", "5n2", "6n1", "7n"]
     posicionesPeon = ["P7", "1P6", "2P5", "3P4", "4P3", "5P2", "6P1", "7P"]
-    return random.choice(posicionesCaballo) + "/8/8/8/8/8/" + random.choice(posicionesPeon) + "/8 w KQkq - 0 1"
+    return random.choice(posicionesCaballo), random.choice(posicionesPeon) 
+
 
 def main():
     pygame.init()
-    grafo = crearGrafo()
+    grafo = crear_grafo()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Ajedrez')
-    fichaSeleccionada = None
+    ficha_seleccionada = None
     seleccionado = 0
-    board = chess.Board(getNewPosicion())
+    n, p = GetNewPosicion()
+    pos = n+"/8/8/8/8/8/"+ p +"/8 w KQkq - 0 1"
+    board = chess.Board(pos)
 
     running = True
     while running:
@@ -141,48 +149,60 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  
                     mouse_x, mouse_y = pygame.mouse.get_pos()
-                    # Calculate the position of the square on the board
+                    # Calcular la posición de la casilla en el tablero
                     col = mouse_x // SQUARE_SIZE
                     row = mouse_y // SQUARE_SIZE
-                    columnLetter = getColumnLetter(col)
-                    rowNumber = 8 - row
-                    square = str(columnLetter) + str(rowNumber)
-                    ficha = board.piece_at(chess.parse_square(square))
-                    if fichaSeleccionada is None:
+                    column_letter = get_column_letter(col)
+                    row_number = 8 - row
+                    cuadrado = str(column_letter) + str(row_number)
+                    ficha = board.piece_at(chess.parse_square(cuadrado))
+                    if ficha_seleccionada is None:
                         if ficha is not None:
                             if ficha.color == board.turn:
                                 if seleccionado == 0:
                                     pygame.draw.rect(screen, BLUE, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-                                    posibles =movimientosFicha(board)
+                                    posibles = movimientos_ficha(board)
+                                    for rows, cols in posibles:
+                                        pygame.draw.rect(screen, GREEN , ((cols )* SQUARE_SIZE, (7-rows) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                                        pygame.draw.rect(screen, BLACK , ((cols )* SQUARE_SIZE, (7-rows) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 1)
+                                    ficha_seleccionada = ficha
                                     seleccionado = 1
-                                    fichaSeleccionada = ficha
                                 elif seleccionado == 1:
-                                    if (row, col) in posibles:
-                                        moverPieza(grafo, board, row, col)
-                                        seleccionado = 0
-                                        fichaSeleccionada = None
-                                        posibles = []
-                                    else:
-                                        seleccionado = 0
-                                        fichaSeleccionada = None
-                                        posibles = []
+                                    pygame.draw.rect(screen, WHITE, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                                    seleccionado = 0
+                                    ficha_seleccionada = None
                     else:
-                        if (row, col) in posibles:
-                            moverPieza(grafo, board, row, col)
+                        if ficha == ficha_seleccionada:
+                            pygame.draw.rect(screen, WHITE, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                             seleccionado = 0
-                            fichaSeleccionada = None
-                            posibles = []
+                            ficha_seleccionada = None
                         else:
-                            seleccionado = 0
-                            fichaSeleccionada = None
-                            posibles = []
+                            if mover_pieza(grafo,board,row,col):
+                                pygame.draw.rect(screen, WHITE, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                                seleccionado = 0
+                                ficha_seleccionada = None
 
-        screen.fill(WHITE)
-        drawBoard(screen)
-        drawPieces(screen, board)
-        pygame.display.update()
-
+        if seleccionado == 0:
+            screen.fill(WHITE)
+            draw_board(screen)
+        draw_pieces(screen, board)
+        pygame.display.flip()
+        if ganador(board):
+            ganadores = "Negras"
+            if not board.turn:
+                ganadores = "Blancas"
+            mostrar_victoria(screen, ganadores)
+            pygame.display.flip()  
+            pygame.time.delay(2000)  
+            running = False
+        elif stalemate(board):
+            mostrar_stalemate(screen)
+            pygame.display.flip()  
+            pygame.time.delay(2000) 
+            running = False
     pygame.quit()
+    mostrar_grafo(grafo)
+    
 
 if __name__ == "__main__":
     main()
